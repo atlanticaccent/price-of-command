952e37d40f241c1e1e4068dfe90efc0c36b4aa82
Iain Laird <iain@laird.org>
2024-03-01 16:18:16 +0000

    WIP

--- src/com/price_of_command/market_interaction/ScarTreatmentEvent.kt
+++ src/com/price_of_command/market_interaction/ScarTreatmentEvent.kt
@@ -1,7 +1,17 @@
 package com.price_of_command.market_interaction
 
-import com.fs.starfarer.api.impl.campaign.intel.bar.events.BaseBarEvent
+import com.fs.starfarer.api.campaign.econ.MarketAPI
+import com.fs.starfarer.api.impl.campaign.intel.bar.events.BaseBarEventWithPerson
+import com.fs.starfarer.api.impl.campaign.intel.bar.events.historian.HistorianBarEvent
+import com.fs.starfarer.api.util.Misc
 
-class ScarTreatmentEvent : BaseBarEvent() {
+class ScarTreatmentEvent : BaseBarEventWithPerson() {
+    override fun shouldShowAtMarket(market: MarketAPI?): Boolean {
+        regen(market)
+        return Misc.random.nextFloat() > HistorianBarEvent.PROB_TO_SHOW && super.shouldShowAtMarket(market)
+    }
 
+    override fun getPersonRank(): String = "doctor"
+
+    override fun getPersonPost(): String = ""
 }
